6197a035155e3123ae3986d896342b707ac2c2ff
Serdiuk-Serhii <114324858+Serdiuk-Serhii@users.noreply.github.com>
2023-03-12 17:19:54 +0300

    ref

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = createCards(galleryItems);
+
+galleryEl.addEventListener("click", onElementOfGalleryClick);
+
+function onElementOfGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+}
+
+function createCards(colection) {
+  return colection
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
-
+// import player from "@vimeo/player";
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+    
+player.on('timeupdate', () => {
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds)
+}).catch(function(error) {
+    // an error occurred
+});
+    
+});
